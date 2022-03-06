82d70875970b4a875222b91acc4cb023782e5207
LukenProgramming <100077230+LukenProgramming@users.noreply.github.com>
2022-03-06 12:11:01 +0000

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,2 +1,9 @@
 import statistics
-statistics.mean([1, 2, 3, 4, 5])
+# This is our input
+a = int(input("How many numbers are you using? "))
+n = list(map(int, input("What are you're numbers ").strip().split()))
+print(n)
+#These are the results
+print("Mean: ", statistics.mean(n))
+print("Mode: ", statistics.mode(n))
+print("Median: ", statistics.median(n))
